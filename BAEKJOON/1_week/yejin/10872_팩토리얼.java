cc4186df288caba25fa9a633301ff6521e66cd8a
Yejin-Ha <hyj1004mu@naver.com>
2022-05-02 18:19:17 +0900

    for loop, 재귀함수 풀이 추가

--- BAEKJOON/1_week/yejin/10872_팩토리얼.java
+++ BAEKJOON/1_week/yejin/10872_팩토리얼.java
@@ -1,3 +1,20 @@
-public int solution(int n){
-    return 1;
+// solution 1) 반복문 사용
+public static int solution1(int n) {
+    if (n == 0)
+        return 1;
+
+    int result = 1;
+    for (int i = n; i > 1; i--) {
+        result *= i;
+    }
+    return result;
+}
+
+// solution 2) 재귀함수
+public static int solution2(int n) {
+    if (n == 1) {
+        return n;
+    } else {
+        return n * solution2(n - 1);
+    }
 }
