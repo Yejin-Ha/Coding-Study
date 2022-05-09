60f9deeb8fac27041169e5a7f559367acd40d575
Yejin-Ha <hyj1004mu@naver.com>
2022-05-09 16:03:04 +0900

    upload 풀이 파일

---
+++ BAEKJOON/1_week/yejin/Q1001.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Q1001{
+    public static int solution() {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        
+        sc.close();
+        return a - b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution());    
+    }
+}
+
---
+++ BAEKJOON/1_week/yejin/Q2439.java
@@ -0,0 +1,16 @@
+public class Q2439 {
+    public static void solution(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = n - i - 1; j > 0; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        solution(5);
+    }
+}
