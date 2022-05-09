f728776209184fe804722b1bcdc52985bf7f5e97
da11un <daeun32174@gmail.com>
2022-05-09 17:19:18 +0900

    upload 1주차 문제풀이

--- BAEKJOON/1_week/daeun/Q1001.java
+++ BAEKJOON/1_week/daeun/Q1001.java
@@ -1,10 +1,16 @@
-public static int solution(int n1, int n2) {
+public class Q1001 {
+    public static int solution(int n1, int n2) {
 
-    int result = 0;
-    if (n1 > 0 && n1 < 10) {
-        if (n2 > 0 && n2 < 10) {
-            result = n1 - n2;
+        int result = 0;
+        if (n1 > 0 && n1 < 10) {
+            if (n2 > 0 && n2 < 10) {
+                result = n1 - n2;
+            }
         }
-    } 
-    return result;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(3, 4));
+    }
 }
--- BAEKJOON/1_week/daeun/Q2439.java
+++ BAEKJOON/1_week/daeun/Q2439.java
@@ -1,11 +1,18 @@
-public static void solution(int n) {
-    for (int i = n; i > 0; i--) {
-        for (int j = i - 1; j > 0; j--) {
-            System.out.print(" ");
+public class Q2439 {
+    public static void solution(int n) {
+        for (int i = n; i > 0; i--) {
+            for (int j = i - 1; j > 0; j--) {
+                System.out.print(" ");
+            }
+            for (int k = n; k >= i; k--) {
+                System.out.print("*");
+            }
+            System.out.println();
         }
-        for (int k = n; k >= i; k--) {
-            System.out.print("*");
-        }
-        System.out.println();
     }
+
+    public static void main(String[] args) {
+        solution(5);
+    }
+
 }
