dfd6c14fce59407ae1c577a805e589fe968ff482
SHJ980818 <yaeleetung@naver.com>
2022-05-09 16:06:45 +0900

    풀이 파일 upload

--- BAEKJOON/1_week/hojoung/1001_A-B.java
+++
@@ -1,9 +0,0 @@
-public static void solution(int n) {
-    Scanner scan = new Scanner(System.in);
-    System.out.println("A를 입력하세요");
-    int A = scan.nextInt();
-    System.out.println("B를 입력하세요");
-    int B = scan.nextInt();
-    
-    System.out.println(A-B);
-}
