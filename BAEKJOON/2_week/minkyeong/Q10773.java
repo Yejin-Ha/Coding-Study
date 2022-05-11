98b07ae38dc820e2a27855897a53af000ac390d7
mki1105 <mki1105@naver.com>
2022-05-11 21:31:52 +0900

    2주차 풀이중

---
+++ BAEKJOON/2_week/minkyeong/Q10773.java
@@ -0,0 +1,5 @@
+public class   Q10773{
+    public static int solution
+    //스택사용
+
+    }
--- BAEKJOON/2_week/minkyeong/Q2164.java
+++ BAEKJOON/2_week/minkyeong/Q2164.java
@@ -1,1 +1,26 @@
-System.out.println("밍");
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class  Q2164{
+    public static int solution(int n){
+        Queue<Integer> que = new LinkedList<>();
+            
+            //값 넣기
+            for(int i = 1; i <= n; i++ ){
+                que.add(i);
+            }
+            //카드? 1이 될 때까지
+            while(true){
+                que.poll();//1
+                que.add(que.poll()); //2번추출
+                if(que.size() == 1 ){
+                    break;
+                }
+            }
+            return que.poll();
+    }
+    public static void main(String[] args) {
+        System.err.println(solution(6));
+        
+    }
+}   
