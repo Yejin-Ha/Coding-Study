80ca4de1b34da674950f041b470750c358c59c34
mki1105 <mki1105@naver.com>
2022-05-16 15:33:42 +0900

    2week

--- BAEKJOON/2_week/minkyeong/Q10773.java
+++ BAEKJOON/2_week/minkyeong/Q10773.java
@@ -1,5 +1,31 @@
-public class   Q10773{
-    public static int solution
-    //스택사용
+import java.util.Scanner;
+import java.util.Stack;
 
+public class   Q10773{
+    //스택사용
+    //public static int solution(){
+     public static void main(String[] args){
+    Stack<Integer> stack = new Stack<>();
+	Scanner scan = new Scanner(System.in);	
+    int k = scan.nextInt(); //입력개수
+		
+    for(int i = 0; i <= k; i++) {
+        int num = scan.nextInt(); // 정수 입력할 것
+        // stack.push(i);
+    
+	if(num == 0) {	//정수가 0이면 스택에서 맨 위 숫자 제거
+		stack.pop();
+	}
+	else {	// 아니면 정수 추가 
+		stack.push(num);
+	}
+}
+ 
+int sum = 0;
+for(int s : stack) {	// 스택에 담기 정수 합
+	sum += s;
+}
+    //public static void main(String[] args){
+        System.out.println(sum);
     }
+}
--- BAEKJOON/2_week/minkyeong/Q2164.java
+++ BAEKJOON/2_week/minkyeong/Q2164.java
@@ -9,7 +9,7 @@ public class  Q2164{
             for(int i = 1; i <= n; i++ ){
                 que.add(i);
             }
-            //카드? 1이 될 때까지
+            //카드가 1이 될 때까지
             while(true){
                 que.poll();//1
                 que.add(que.poll()); //2번추출
@@ -20,7 +20,7 @@ public class  Q2164{
             return que.poll();
     }
     public static void main(String[] args) {
-        System.err.println(solution(6));
+        System.out.println(solution(6));
         
     }
 }   
