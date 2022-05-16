e459b8c493037fc81b69725bc64dd6873bdbfaf0
mki1105 <mki1105@naver.com>
2022-05-16 19:09:43 +0900

    2주 3주 풀이 수정

--- BAEKJOON/2_week/minkyeong/Q2164.java
+++ BAEKJOON/2_week/minkyeong/Q2164.java
@@ -1,27 +1,29 @@
 import java.util.LinkedList;
 import java.util.Queue;
 
-public class  Q2164{
+public class Q2164{
     public static int solution(int n){
+        //큐 선언
         Queue<Integer> que = new LinkedList<>();
             
             //값 넣기
             for(int i = 1; i <= n; i++ ){
                 que.add(i);
             }
-            //카드가 1이 될 때까지
+            //
             while(que.size() != 1){
-                que.poll();//1
-                que.add(que.poll()); //2번추출
-                
+                que.poll(); // 1을 추출
+                que.add(que.poll());
+                //if(que.size() == 1 ){
+                // break;
             }
             return que.poll();
     }
     public static void main(String[] args) {
         System.out.println(solution(6));
         
-    } // 이전 코드에 while(true), if문을 사용할 경우에는
-     // poll되어 있는 상태에서 또 poll을 하려고 하고 add 하려고 하기 때문에
-     //조건 while안에 사용하기
-     
+    }// 이전 코드에 while(true), if문을 사용할 경우에는
+     // 아무것도 없는 상태에서 추출하고 add를 하려고 해서 1이 될 때까지 while문이 계속 실행됨.
+     // 조건이 안맞아서 컴파일 에러
+     // 조건 que.size()!=1 로 while문 활용하기
 }   
