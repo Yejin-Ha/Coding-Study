7fcdd51e077c4209a4787964f0919403c1014db0
Yejin-Ha <hyj1004mu@naver.com>
2022-05-30 15:51:20 +0900

    풀이과정 upload

--- BAEKJOON/week04/yejin/Q10866.java
+++ BAEKJOON/week04/yejin/Q10866.java
@@ -4,11 +4,12 @@ import javax.lang.model.element.Element;
 
 public class Q10866 {
     public static void main(Stringp[] args){
+        Deque<String> deque = new ArrayDeque<>();
         Scanner scan = new Scanner(System.in);
         int length = Integer.parseInt(scan.nextLine());
         for (int i = 0; i < length; i++){
             String[] menu = scan.nextLine().split(" ");
-
+            
         }
     }
 }
@@ -20,4 +21,6 @@ public class Q10866 {
  * 특히 한쪽으로만 입력 가능하도록 설정한 덱을 스크롤(scroll)이라고 하며, 한쪽으로만 출력 가능하도록 설정한 덱을 셸프(shelf)라고
  * 한다.
  * 자바에서의 덱은 인터페이스로 구현되었다.
+ * 
+ * Switch 문 사용하여 풀면 된다.
  */
