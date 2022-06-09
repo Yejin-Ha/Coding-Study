f2b3321d68c89d44c18fa920df62fad2f910c6aa
Yejin-Ha <hyj1004mu@naver.com>
2022-06-09 16:36:57 +0900

    Q 1929 문제 풀이 & 코드 제출

---
+++ BAEKJOON/week06/yejin/Q1929.java
@@ -0,0 +1,34 @@
+/*
+ * #### 제출
+ * 맞았습니다!!
+ */
+
+import java.io.*;
+import java.util.*;
+
+public class Q1929 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int start = Integer.parseInt(st.nextToken());
+        int end = Integer.parseInt(st.nextToken());
+
+        boolean[] prime = new boolean[end + 1];
+        prime[0] = prime[1] = true;
+
+        for (int i = 2; i <= end; i++) {
+            if (prime[i]) {
+                continue;
+            }
+            for (int j = i * 2; j <= end; j += i) {
+                prime[j] = true;
+            }
+        }
+        for (int i = start; i < end + 1; i++) {
+            if (!prime[i])
+                System.out.println(i);
+        }
+        br.close();
+    }
+}
