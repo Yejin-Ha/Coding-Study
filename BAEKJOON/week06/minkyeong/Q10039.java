d99e84f41ae47b542ba1341697a65e43da855743
mki1105 <mki1105@naver.com>
2022-06-13 08:45:38 +0900

    week06 풀이

--- BAEKJOON/week06/minkyeong/Q10039.java
+++ BAEKJOON/week06/minkyeong/Q10039.java
@@ -1,3 +1,28 @@
-public class Q10039{
-    
+import java.util.Scanner;
+
+public class Q10039 {
+    // 평균 점수(원섭-세희-상근-숭-강수 순서대로)
+    // 점수는 0점 이상, 100점 이하인 5의 배수, 평균점수는 정수
+    // 5명의 점수가 주어졌을 때, 평균 점수 구하는 프로그램
+    // 40점 이상인 학생들은 그대로 성적 인정, 40점 미만은 보충학습
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int score = 0; //점수
+        int sum = 0; // 합계
+
+        for(int i = 0; i < 5; i++){
+            score = scan.nextInt();
+            
+            if(score > 40){
+                sum += score;
+            }
+            else {
+                sum += 40;
+            }
+        }
+        System.out.println(sum/5);
+    }
+
 }
+
